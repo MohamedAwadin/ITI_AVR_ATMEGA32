bc0ccf8b68b8ea519f105ddbae133c4cd6ab422a
MohamedAwadin <mohamedawadin01@gmail.com>
2024-12-22 23:39:56 +0200

    Finish

---
+++ APPS/LABS/TASK1/build/CMakeFiles/burn_Task1.elf.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/burn_Task1.elf"
+  "Task1.elf.hex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/burn_Task1.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ APPS/LABS/TASK2/build/CMakeFiles/flash_Task2.elf.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flash_Task2.elf"
+  "Task2.elf.hex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/flash_Task2.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
